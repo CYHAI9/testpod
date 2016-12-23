e6449f40b5b3a6826c1ad85d9cb71e8021dc6732
又是一个大叔 <3069126500@qq.com>
2016-12-23 11:57:17 +0800

    第一次上传

---
+++ checkphoneNum/NSString+checkNum.h
@@ -0,0 +1,15 @@
+//
+//  NSString+checkNum.h
+//  wisdomSite
+//
+//  Created by Macx on 16/4/14.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (checkNum)
+
+-(BOOL)checkPhoneNumInput;
+
+@end
